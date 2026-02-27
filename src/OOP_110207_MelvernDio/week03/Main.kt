547fe297ccb4f5b8a127db951acdc010bfae2ac8
MelvernDio <dpisnoob@gmail.com>
2026-02-27 09:16:10 +0700

    week03: fix setter using backing field

--- src/OOP_110207_MelvernDio/week03/Main.kt
+++ src/OOP_110207_MelvernDio/week03/Main.kt
@@ -2,6 +2,12 @@ package OOP_110207_MelvernDio.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000 // Ini akan meledakkan program Anda
+
+    println("--- Tes Gaji Positif (Benar) ---")
+    e.salary = 5000
+    println("Gaji Budi sekarang: ${e.salary}")
+
+    println("\n--- Tes Gaji Negatif (Salah) ---")
+    e.salary = -1000
+    println("Gaji Budi sekarang: ${e.salary}")
 }
