1d9e8d31895354bb10550eb57f9ae53c0ad36059
MelvernDio <dpisnoob@gmail.com>
2026-02-27 09:29:33 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/OOP_110207_MelvernDio/week03/Weapon.kt
@@ -0,0 +1,27 @@
+package OOP_110207_MelvernDio.week03
+
+class Weapon(val name: String) {
+
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Peringatan: Damage tidak boleh negatif! Nilai tetap dipertahankan.")
+
+            } else if (value > 1000) {
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() {
+            return if (damage > 800) {
+                "Legendary"
+            } else if (damage > 500) {
+                "Epic"
+            } else {
+                "Common"
+            }
+        }
+}
