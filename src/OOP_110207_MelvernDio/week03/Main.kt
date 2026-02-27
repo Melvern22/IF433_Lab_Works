4fa3e413a9359094f7cc8373c216a7f10900d87c
MelvernDio <dpisnoob@gmail.com>
2026-02-27 09:32:14 +0700

    week03: implement and test Weapon class in main

--- src/OOP_110207_MelvernDio/week03/Main.kt
+++ src/OOP_110207_MelvernDio/week03/Main.kt
@@ -9,4 +9,20 @@ fun main() {
     e.increasePerformance()
 
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n=== TES TUGAS MANDIRI: WEAPON ===")
+
+    // Buat objek Weapon
+    val pedang = Weapon("Excalibur")
+
+    // Coba set damage ke -50 (Harus gagal dan print peringatan)
+    pedang.damage = -50
+    println("Damage pedang sekarang: ${pedang.damage}")
+
+    // Coba set damage ke 9999 (Harus dipaksa jadi 1000)
+    pedang.damage = 9999
+    println("Damage pedang sekarang: ${pedang.damage}")
+
+    // Print Tier-nya (Karena 1000 > 800, harusnya Legendary)
+    println("Tier senjata ${pedang.name}: ${pedang.tier}")
 }
