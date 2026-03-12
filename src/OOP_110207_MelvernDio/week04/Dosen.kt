059663e8b2030acd5af0c35376985afc01b4dcc3
MelvernDio <dpisnoob@gmail.com>
2026-03-12 11:43:56 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/OOP_110207_MelvernDio/week04/Dosen.kt
@@ -0,0 +1,14 @@
+package OOP_001_MelvernDio.week05
+
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+
+    // WAJIB di-override karena fungsi bekerja() bersifat abstract di Parent
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    // Fungsi unik/spesifik yang hanya dimiliki Dosen
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
