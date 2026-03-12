96e3945c9c0b6b0500a7aaa736816563348dc3f9
MelvernDio <dpisnoob@gmail.com>
2026-03-12 11:49:52 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/OOP_110207_MelvernDio/week04/Admin.kt
@@ -0,0 +1,12 @@
+package OOP_001_MelvernDio.week05
+
+class Admin(nama: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    fun doAdminWork() {
+        println("[$nama] sedang merekap data absensi mahasiswa.")
+    }
+}
