77d644e117e10035a7ba9f3acc9479c493388697
MelvernDio <dpisnoob@gmail.com>
2026-03-12 12:24:46 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/OOP_110207_MelvernDio/week04/Main.kt
+++ src/OOP_110207_MelvernDio/week04/Main.kt
@@ -3,11 +3,13 @@ package OOP_001_MelvernDio.week05
 fun main() {
     val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
     val admin1 = Admin(nama = "Bu Siti")
+
     val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
 
     println("=== AKTIVITAS PEGAWAI ===")
     for (pegawai in daftarPegawai) {
         pegawai.bekerja()
+
         when (pegawai) {
             is Dosen -> {
                 println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
@@ -20,4 +22,16 @@ fun main() {
         }
         println("-------------------------")
     }
+    println("\n=== PENGUJIAN MATH HELPER (OVERLOADING) ===")
+
+    val mathHelper = MathHelper()
+
+    val luasPersegi = mathHelper.hitungLuas(5)
+    println("Luas Persegi (sisi 5) = $luasPersegi")
+
+    val luasPersegiPanjang = mathHelper.hitungLuas(4, 6)
+    println("Luas Persegi Panjang (4 x 6) = $luasPersegiPanjang")
+
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+    println("Luas Lingkaran (jari-jari 7.0) = $luasLingkaran")
 }
