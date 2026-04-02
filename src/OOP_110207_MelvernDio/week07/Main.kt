96e5280d2bf919a4742d4cb579232a65a8edf416
MelvernDio <dpisnoob@gmail.com>
2026-04-02 15:25:02 +0700

    week07: implement companion object as factory in NetworkClient

--- src/OOP_110207_MelvernDio/week07/Main.kt
+++ src/OOP_110207_MelvernDio/week07/Main.kt
@@ -1,7 +1,5 @@
 package OOP_110207_MelvernDio.week07
 
 fun main() {
-    // Baris ini akan error (garis merah) karena konstruktornya private.
-    // Dibiarkan saja error, karena memang ini tujuannya.
-    val client = NetworkClient("https://api.umn.ac.id")
+    val client = NetworkClient.createClient()
 }
