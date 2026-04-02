5d4e50119733f18961f9a38222331b53610ad2ac
MelvernDio <dpisnoob@gmail.com>
2026-04-02 16:10:15 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/OOP_110207_MelvernDio/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package OOP_110207_MelvernDio.week07
+
+sealed class BattleState {
+    data class MonsterEncounter(val monsterName: String) : BattleState()
+    data class LootDropped(val item: GameItem) : BattleState()
+    data class GameOver(val reason: String) : BattleState()
+    object SafeZone : BattleState() // object karena tidak membawa payload (data)
+}
